aa5ed6674b50e67825844a2e1c975071bfb2de7d
Henrik Tøn Løvhaug <henrik.lovhaug@gmail.com>
2024-05-31 22:15:28 +0200

    Update version

--- lua/preview.lua
+++ lua/preview.lua
@@ -9,7 +9,7 @@ local function install()
 		err("curl is required to install this plugin")
 	end
 
-	local version = "0.7.2"
+	local version = "0.8.0"
 
 	local install_script = "curl --proto '=https' --tlsv1.2 -LsSf https://github.com/henriklovhaug/md-tui/releases/download/v"
 		.. version
